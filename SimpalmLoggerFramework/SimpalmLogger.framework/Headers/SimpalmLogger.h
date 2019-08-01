0eb73b42ed73a4fcfc3741252085ec4aa083c4a6
Aman Sinha <aman@simpalm.com>
2019-08-01 22:05:37 +0530

    Framework Upoaded

---
+++ SimpalmLoggerFramework/SimpalmLogger.framework/Headers/SimpalmLogger.h
@@ -0,0 +1,19 @@
+//
+//  SimpalmLogger.h
+//  SimpalmLogger
+//
+//  Created by Aman Sinha on 30/07/19.
+//  Copyright © 2019 Simpalm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpalmLogger.
+FOUNDATION_EXPORT double SimpalmLoggerVersionNumber;
+
+//! Project version string for SimpalmLogger.
+FOUNDATION_EXPORT const unsigned char SimpalmLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpalmLogger/PublicHeader.h>
+
+
